5f7366625b82504118e523e6f937089c06bbf5bb
PranjalMandhan <138661819+pranjalmandhan@users.noreply.github.com>
2026-01-15 22:21:28 +0500

    Update cleaning.sql

--- scripts/cleaning.sql
+++ scripts/cleaning.sql
@@ -1,1 +1,14 @@
-
+WITH UniqueOrders AS (
+    SELECT DISTINCT * FROM raw_sales_data
+)
+SELECT 
+    order_id,
+    COALESCE(UPPER(customer_name), 'UNKNOWN') AS customer_name,
+    product_name,
+    CASE 
+        WHEN order_date LIKE '%/%' THEN '2026-02-01' 
+        ELSE order_date 
+    END AS clean_order_date,
+    ABS(amount) AS clean_amount, 
+    status
+FROM UniqueOrders;
